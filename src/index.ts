bd14695ad67bf1181d3415a07eb834fe55d8e64c
Divyansh Sharma <divyansh.sharma@iamtechie.com>
2025-11-13 12:13:10 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,9 @@ app.use(`/${baseUrl}/wishlist`, wishlistRoutes);
 app.use(`/${baseUrl}/auth`, authRoutes);
 
 const PORT = process.env.PORT || 5000;
-
+app.get("/", (_, res) => {
+  return res.status(200).json("Server is running");
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
